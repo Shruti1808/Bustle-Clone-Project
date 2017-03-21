3305ad44108b673f1ebcd7ac31ef042333c6f214
Grinil Khanna and Shruti Priya <grinil+shruti@>
2017-03-21 14:41:24 -0700

    Add an update function - styling needs to b done and admin accessibility.

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,6 +13,16 @@ export default Ember.Route.extend({
       this.transitionTo('index');
     },
 
+    update (article, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          article.set(key,params[key]);
+        }
+      });
+      article.save();
+      this.transitionTo('index');
+    },
+
     destroyArticle(article){
       article.destroyRecord();
       this.transitionTo('index');
