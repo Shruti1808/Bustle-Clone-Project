9d1d34f9e0ace9a19251131de7b4e43679c2fc52
Grinil Khanna and Shruti Priya <grinil+shruti@>
2017-03-21 13:43:16 -0700

    add code to delete a particular article

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return articles;
+    return this.store.findAll('article');
   },
+
+  actions: {
+    destroyArticle(article){
+      article.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
